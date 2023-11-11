d53b45e2e2977400c7ac87bcd0fe42f8e4872710
abedini <amirmohammad.abedini@gmail.com>
2023-11-11 08:05:51 +0000

    Fix: J-meter deployment on remote cluster

---
+++ scenario/engage/load_test.sh
@@ -0,0 +1,9 @@
+slave_array=(10.233.105.152 10.233.125.34); index=2 && while [ ${index} -gt 0 ]; do for slave in ${slave_array[@]}; do if echo 'test open port' 2>/dev/null > /dev/tcp/${slave}/1099; then echo ${slave}' ready' && slave_array=(${slave_array[@]/${slave}/}); index=$((index-1)); else echo ${slave}' not ready'; fi; done; echo 'Waiting for slave readiness'; sleep 2; done
+echo "Installing needed plugins for master"
+cd /opt/jmeter/apache-jmeter/bin
+sh PluginsManagerCMD.sh install-for-jmx engage.jmx
+echo "Done installing plugins, launching test"
+jmeter -Ghost= -Gport= -Gprotocol= -Gthreads= -Gduration= -Grampup=  --logfile /report/engage.jmx_2023-11-08_172516.jtl --nongui --testfile engage.jmx -Dserver.rmi.ssl.disable=true --remoteexit --remotestart 10.233.105.152,10.233.125.34 >> jmeter-master.out 2>> jmeter-master.err &
+trap 'kill -10 1' EXIT INT TERM
+java -jar /opt/jmeter/apache-jmeter/lib/jolokia-java-agent.jar start JMeter >> jmeter-master.out 2>> jmeter-master.err
+echo "Starting load test at : Wed  8 Nov 2023 17:25:16 GMT" && wait
